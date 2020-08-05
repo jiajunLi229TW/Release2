cc6b06d0a1ae42699c75ad53110eaf3aed220ed5
jiajunLi229TW <jiajun.li1@thoughtworks.com>
2020-08-05 21:11:27 +0800

    [JiajunLi] should hide button before login.

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 const selectedMovie = document.getElementById("selectedBook");
 const LoginArea = document.getElementById("loginContainer");
-
+const selectionArea = document.getElementById("select");
+const informationButton = document.getElementById("informationCheck");
 function checkout(buttonStatus) {
       const filmName = buttonStatus.nextSibling.innerHTML;
       selectedMovie.innerHTML = filmName;
@@ -9,12 +10,13 @@ function checkout(buttonStatus) {
 function validate() {
 const userNameValue = document.getElementById("userName").value;
 const passWordValue = document.getElementById("passWord").value;
-const selectionArea = document.getElementById("select");
+
       if(userNameValue == "1" && passWordValue == "1" )
    {
        alert( "validation succeeded");
        LoginArea.style.display = "none";
        selectionArea.style.display = "flex";
+       informationButton.style.display = "flex";
    }
    else
    {
