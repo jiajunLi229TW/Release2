01ba90416bac2048b7b561564da95c7580b9b045
jiajunLi229TW <jiajun.li1@thoughtworks.com>
2020-08-05 21:33:56 +0800

    [JiajunLi] style: adujusted format and style.

--- library.js
+++ library.js
@@ -3,36 +3,29 @@ const LoginArea = document.getElementById("loginContainer");
 const selectionArea = document.getElementById("select");
 const informationButton = document.getElementById("informationCheck");
 const informationCard = document.getElementById("card");
+
 function checkout(buttonStatus) {
-      const filmName = buttonStatus.nextSibling.innerHTML;
-      selectedMovie.innerHTML = filmName;
-};
+  const filmName = buttonStatus.nextSibling.innerHTML;
+  selectedMovie.innerHTML = filmName;
+}
 
 function validate() {
-const userNameValue = document.getElementById("userName").value;
-const passWordValue = document.getElementById("passWord").value;
+  const userNameValue = document.getElementById("userName").value;
+  const passWordValue = document.getElementById("passWord").value;
 
-      if(userNameValue == "1" && passWordValue == "1" )
-   {
-       alert( "validation succeeded");
-       LoginArea.style.display = "none";
-       selectionArea.style.display = "flex";
-       informationButton.style.display = "flex";
-   }
-   else
-   {
-       alert( "validation failed");
-   }
+  if (userNameValue == "1" && passWordValue == "1") {
+    alert("validation succeeded");
+    LoginArea.style.display = "none";
+    selectionArea.style.display = "flex";
+    informationButton.style.display = "flex";
+  } else {
+    alert("validation failed");
+  }
 }
 
 function checkInformation() {
-      if(informationButton.clicked = true) {
-            alert("hello");
-            informationCard.style.display = "block";
-            informationButton.style.display = "none";
-      }
-      else {
-            alert("false");
-      }
-      //if(document. getElementById('button'). clicked == true)
+  if ((informationButton.clicked = true)) {
+    informationCard.style.display = "block";
+    informationButton.style.display = "none";
+  } 
 }
