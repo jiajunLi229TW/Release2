763e9209e31c3ca64247384ab40b57f057acc26c
jiajunLi229TW <jiajun.li1@thoughtworks.com>
2020-08-05 20:26:05 +0800

    [JiajunLi] feat: should validate user login.

--- library.js
+++ library.js
@@ -1,5 +1,19 @@
 const selectedMovie = document.getElementById("selectedBook");
+
 function checkout(buttonStatus) {
       const filmName = buttonStatus.nextSibling.innerHTML;
       selectedMovie.innerHTML = filmName;
 };
+
+function validate() {
+const userNameValue = document.getElementById("userName").value;
+const passWordValue = document.getElementById("passWord").value;
+      if(userNameValue == "admin" && passWordValue == "admin" )
+   {
+       alert( "validation succeeded");
+   }
+   else
+   {
+       alert( "validation failed");
+   }
+}
